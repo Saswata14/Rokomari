ea3770e4631ea3a593733691556741cc7cfb77d5
aryaman0777 <aryamanpd.bkp@gmail.com>
2025-11-06 23:44:14 +0530

    login and cookie created

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import {login, employeeRegister, employerRegister} from "../controllers/authController.js"
+import {employeeRegister, employerRegister, employeeLogin} from "../controllers/authController.js"
 
 const router = express.Router();
 
 router.post('/registerEmployee',employeeRegister);
 router.post('/registerEmployer',employerRegister);
-router.post('/login',login);
+router.post('/loginEmployee',employeeLogin);
 
 export default router;
