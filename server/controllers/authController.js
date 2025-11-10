6374db89b787a620d213a8bf41b32c1b1498cc59
Saswata Patra <saswatapatra6@gmail.com>
2025-11-10 20:07:48 +0530

    bugged is fixed -both employee and employer login is working

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -26,9 +26,7 @@ export const employeeRegister = async(req,res)=>{
         await newEmployee.save();
         
 
-
-
-        const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET, {expiresIn: '7d'}); 
+        const token = jwt.sign({id: newEmployee._id}, process.env.JWT_SECRET, {expiresIn: '7d'}); 
         
         res.cookie("token", token, {
             httpOnly: true,
@@ -57,14 +55,14 @@ export const employerRegister = async(req,res)=>{
         }
         const hashPassword= await bcrypt.hash(password,10)
 
-        const newUser = new employerModel({
+        const newEmployer = new employerModel({
             name,email,password:hashPassword,phone,gender,id
         });
 
-        await newUser.save();
+        await newEmployer.save();
         return res.status(200).json({success:true, message:"user created successfully"});
 
-        const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET, {expiresIn: '7d'}); 
+        const token = jwt.sign({id: newEmployer._id}, process.env.JWT_SECRET, {expiresIn: '7d'}); 
         
         const cookie = res.cookie("token", token, {
             httpOnly: true,
@@ -92,7 +90,7 @@ export const employeeLogin = async(req,res)=>{
         if(!existingUser){
             return res.status(400).json({success:false, message:"The email does not exists"})
         }
-        const {token} = req.cookies;
+        /*const {token} = req.cookies;
         if(token){
             try {
                 const decodedToken= jwt.verify(token,process.env.JWT_SECRET);
@@ -102,12 +100,12 @@ export const employeeLogin = async(req,res)=>{
                 return res.status(400).json(error.message);
                 
             }
-        }
+        }*/
 
-        const decryptedPass = bcrypt.compare(existingUser.password,password);
+        const decryptedPass = await bcrypt.compare(existingUser.password,password);
 
         if(decryptedPass){
-            const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET, {expiresIn: '7d'}); 
+            const token = jwt.sign({id: existingUser._id}, process.env.JWT_SECRET, {expiresIn: '7d'}); 
         
             const cookie = res.cookie("token", token, {
             secure: process.env.NODE_ENV === "production",
@@ -121,6 +119,56 @@ export const employeeLogin = async(req,res)=>{
              return res.status(400).json({success:false, message:"Wrong Password!!! Try again"});
         
         }
+
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+};
+
+export const employerLogin = async(req,res)=>{
+    try {
+        const {email,password} = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({success:false, message:"all fields needed"});
+        }
+
+        const existingUser = await employerModel.findOne({email});
+
+        if(!existingUser){
+            return res.status(400).json({success:false, message:"The email does not exists"})
+        }
+        /*const {token} = req.cookies;
+        if(token){
+            try {
+                const decodedToken= jwt.verify(token,process.env.JWT_SECRET);
+                return res.status(200).json({success:true, message:"already logged in",userId: decodedToken._id })
+                
+            } catch (error) {
+                return res.status(400).json(error.message);
+                
+            }
+        }*/
+
+        const decryptedPass = await bcrypt.compare(existingUser.password,password);
+
+        if(decryptedPass){
+            const token = jwt.sign({id: existingUser._id}, process.env.JWT_SECRET, {expiresIn: '7d'}); 
+        
+            const cookie = res.cookie("token", token, {
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+            
+        })
+        return res.status(200).json({success:true, message:"Welcome to Rokomari"});
+
+        }else{
+             return res.status(400).json({success:false, message:"Wrong Password!!! Try again"});
+        
+        }
+
+         
 
     } catch (error) {
         return res.status(400).json(error.message);
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {employeeRegister, employerRegister, employeeLogin} from "../controllers/authController.js"
+import {employeeRegister, employerRegister, employeeLogin, employerLogin} from "../controllers/authController.js"
 
 const router = express.Router();
 
 router.post('/registerEmployee',employeeRegister);
 router.post('/registerEmployer',employerRegister);
 router.post('/loginEmployee',employeeLogin);
+router.post('/loginEmployer',employerLogin);
 
 export default router;
